2c81be62aafa2a88689accc45569eaeb742d8063
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-04 20:50:12 -0700

    Setting up error detail

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/model/ErrorDetail.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/model/ErrorDetail.java
@@ -14,6 +14,56 @@ public class ErrorDetail {
     private String detail;
     private String timestamp;
     private String devMessage;
-    private Map<String List<ValidationError>> errors = new HashMap<String, List<ValidationError>>();
+    private Map<String, List<ValidationError>> errors = new HashMap<String, List<ValidationError>>();
 
+    // auto generates getters and setters.
+    // manually edited timestamp
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDevMessage() {
+        return devMessage;
+    }
+
+    public void setDevMessage(String devMessage) {
+        this.devMessage = devMessage;
+    }
+
+    public Map<String, List<ValidationError>> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, List<ValidationError>> errors) {
+        this.errors = errors;
+    }
 }
