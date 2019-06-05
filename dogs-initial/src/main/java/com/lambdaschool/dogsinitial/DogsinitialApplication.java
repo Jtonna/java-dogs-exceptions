df03bbfe2d363e1ce465c0ef68770c6fa3585752
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-04 17:40:25 -0700

    set dog list to public

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DogsinitialApplication
 {
 
-    static DogList ourDogList;
+    public static DogList ourDogList;
+    // setting this to public allows controller to see ourDogList
     public static void main(String[] args)
     {
         ourDogList = new DogList();
